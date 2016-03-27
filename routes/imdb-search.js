b5ebe81cedd993704a7bcc286982129d18933708
Suresh Swamy <email": null>
2016-03-28 10:01:34 +1300

    search api tweaks

--- routes/imdb-search.js
+++ routes/imdb-search.js
@@ -10,38 +10,39 @@ var movieList = { 'list':[]}
 
 router.get('/search/', function (req, res){
   var str = 'jaws'
-  
+
   // var str = JSON.stringyfy(movieName)
 
   var url= 'http://www.imdb.com/find?ref_=nv_sr_fn&q='+str+'&s=all'
 
   console.log ('here is search', str, url)
-  
+
 
   request(url, function (err, response, html) {
 
     if(!err) {
-      
-  
+
+
       var $ = cheerio.load(html);
 
       var  data, img, titleUrl, imgUrl;
       var searchRes = { 'titlesUrl': '', imageUrl:''}
-      
+
       $('.primary_photo').each(function() {
         data = $(this).children();
         titleUrl = data.attr('href');
         imgUrl= $('img', data).attr("src");
 
-        searchRes.titlesUrl = titleUrl
-        searchRes.imageUrl = imgUrl
-       
+        searchRes['titleUrl'].titleUrl
+        searchRes['imgUrl'].imgUrl
+
+
         console.log("here is title and image link", titleUrl)
       })
 
-       movieList['list'].push(searchRes)
+
     }
-    
+
     // fs.appendFile('./data/movieList.json', JSON.stringify(titles, null, ' '), function(err) {
     fs.appendFileSync('./data/movieList.json', JSON.stringify(movieList))
     //   function(err) {
