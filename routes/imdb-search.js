dcaa6543202df920508878e8e4bf79381f79f8d5
sureshnswamy <sureshnswamy@gmail.com>
2016-03-29 01:05:12 +1300

    search api full functional

--- routes/imdb-search.js
+++ routes/imdb-search.js
@@ -6,48 +6,33 @@ var cheerio = require('cheerio');
 var router = express.Router();
 
 //var searchList = {'movies':[{ 'titlesUrl': '', imageUrl:''}]}
-var movieList = { 'list':[]}
 
 router.get('/search/', function (req, res){
-  var str = 'jaws'
-  
-  // var str = JSON.stringyfy(movieName)
-
-  var url= 'http://www.imdb.com/find?ref_=nv_sr_fn&q='+str+'&s=all'
-
-  console.log ('here is search', str, url)
-  
+var searchList = {}
+var str = 'jaws'
+var url= 'http://www.imdb.com/find?ref_=nv_sr_fn&q='+str+'&s=all'
+console.log ('here is search', str, url)
 
   request(url, function (err, response, html) {
-
+    
     if(!err) {
-      
-  
+        
       var $ = cheerio.load(html);
 
       var  data, img, titleUrl, imgUrl;
-      var searchRes = { 'titlesUrl': '', imageUrl:''}
-      
+        
       $('.primary_photo').each(function() {
         data = $(this).children();
-        titleUrl = data.attr('href');
-        imgUrl= $('img', data).attr("src");
-
-        searchRes.titlesUrl = titleUrl
-        searchRes.imageUrl = imgUrl
-       
-        console.log("here is title and image link", titleUrl)
+        titleUrl = data.attr('href')
+        titleTrim = titleUrl.indexOf('?')
+        title= titleUrl.substring(titleTrim,0)
+        imgUrl= $('img', data).attr("src")
+        searchList['titleUrl'] = title
+        searchList['imgUrl'] = imgUrl
+        fs.appendFileSync('./data/movieList.json', JSON.stringify(searchList)+'\n')
+          //console.log("here is title and image link", movieList)
       })
-
-       movieList['list'].push(searchRes)
     }
-    
-    // fs.appendFile('./data/movieList.json', JSON.stringify(titles, null, ' '), function(err) {
-    fs.appendFileSync('./data/movieList.json', JSON.stringify(movieList))
-    //   function(err) {
-    //   console.log('File successfully written to  movieList.json  file');
-    // })
-
     res.send('Your movie serach is here!')
   })
 
